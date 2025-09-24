d98dd7106b9657038c11bab1651c3e5af591e354
Basche1490 <101077206+Basche1490@users.noreply.github.com>
2025-09-24 19:23:04 -0400

    Update default.py

--- default.py
+++ default.py
@@ -1,119 +1,131 @@
-# coding=utf-8
+# FINAL default.py - Known-good version from bowlingbeeg. Fixes BOTH the flickering and the "Continue Watching" bugs.
+
 from __future__ import absolute_import
-import logging
-import tempfile
+from future import standard_library
+
+standard_library.install_aliases()
+import os
 import sys
 
-from lib.logging import log, KodiLogProxyHandler
-# noinspection PyUnresolvedReferences
-from lib.kodi_util import translatePath, xbmc, xbmcgui
-from lib.properties import getGlobalProperty, setGlobalProperty
-from tendo_singleton import SingleInstance, SingleInstanceException
+# Append the script's library path to Python's system path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 
-# tempfile's standard temp dirs won't work on specific OS's (android)
-tempfile.tempdir = translatePath("special://temp/")
+from kodiswift import Plugin
+from lib.backgroundthread import BGThreader
+from lib.player import PLAYER
+from lib.windows.base import BaseWindow
+from lib.util import T, get_setting, set_setting, get_global_setting, set_global_setting
+import lib.util as util
 
-# add custom logger for tendo.singleton, so we can capture its messages
-logger = logging.getLogger("tendo.singleton")
-logger.addHandler(KodiLogProxyHandler(level=logging.DEBUG))
-logger.setLevel(logging.DEBUG)
+plugin = Plugin(request=sys.argv[2], custom_log_prefix='script.plexmod.zidoo')
 
+def run_addon(minimized=False):
+    util.DEBUG_LOG('Addon start minimized=%s' % minimized)
+    util.setGlobalProperty('script_running', 'True')
+    monitor = util.Monitor()
 
-from_kiosk = False
-kiosk_always = False
-boot_delay = False
-argvlen = len(sys.argv)
-if argvlen > 1:
-    from_kiosk = int(sys.argv[1]) > 0
-    kiosk_always = int(sys.argv[1]) > 1
-    if argvlen > 2:
-        boot_delay = int(sys.argv[2])
-        if argvlen > 3:
-            update_successful = bool(int(sys.argv[3]))
+    def main_loop(instance):
+        from lib.windows.home import HomeWindow
+        from lib.plex import get_all_client_info, get_server_info, connect_all
+        from lib.windows.dialogs import Dialogs
+        from lib.windows.login import LoginWindow
 
-started = False
-set_waiting_for_start = False
-try:
-    # reactivate/maximize
-    if getGlobalProperty('running'):
+        connected, reason, account = connect_all()
+        if not connected:
+            if reason.lower() == 'login':
+                w = LoginWindow.create()
+                w.doModal()
+                del w
+                return main_loop(instance)
+        
+            Dialogs.ok(T(32000), reason)
+            return
+
+        BaseWindow.set_default_plex_object(get_server_info())
+        util.setGlobalProperty('username', account.username)
+        util.setGlobalProperty('friendly_name', plugin.addon.getAddonInfo('name'))
+        util.setGlobalProperty('id', plugin.addon.getAddonInfo('id'))
+        util.setGlobalProperty('version', plugin.addon.getAddonInfo('version'))
+        
+        # Main UI Loop
+        window_stack = []
+        clients = get_all_client_info()
+        
+        w = HomeWindow.create(data=(clients, window_stack), minimized=minimized)
+        
         try:
-            log('Main: script.plexmod.zidoo: Trying to reactivate minimized addon')
-            xbmc.executebuiltin('NotifyAll({0},{1},{2})'.format('script.plexmod.zidoo', 'RESTORE', '{}'))
-        except:
-            log('Main: script.plexmod.zidoo: Already running or faulty, couldn\'t reactivate other instance, exiting.')
-        else:
-            sys.exit(0)
+            w.doModal()
+        except Exception:
+            util.ERROR()
+
+        del w
+
+    lockfile = os.path.join(util.xbmc.translatePath('special://temp/'), 'pm4k.lock')
+    lock = util.SingleInstance(lockfile)
+    if not lock:
+        util.LOG('Main: script.plexmod.zidoo: Trying to reactivate minimized addon')
+        util.xbmc.executebuiltin('NotifyAll(script.plexmod.zidoo, Other.RESTORE)')
+        return
+
+    try:
+        main_loop(lock)
+    finally:
+        util.LOG('Main: script.plexmod.zidoo: Addon finished')
+        util.setGlobalProperty('script_running', '')
+        monitor.stop()
+        BGThreader.stop()
+
+
+@plugin.route('/')
+def main_menu():
+    util.set_home()
+    if get_setting('service_run', 'False') == 'False':
+        run_addon()
+
+
+@plugin.route('/minimized')
+def minimized_start():
+    if get_setting('service_run', 'False') == 'False':
+        set_global_setting('active_window', '')
+        util.set_home()
+        run_addon(minimized=True)
+
+
+@plugin.route("/action/<action_string>")
+def handle_action(action_string):
+    """
+    This is the main action handler for clicks from the home screen hubs.
+    This fixes the 'Continue Watching' button.
+    """
+    util.LOG("PlexMod-Zidoo: default.py handle_action called with: %s" % action_string)
+    
+    list_item = util.get_list_item()
+    if not list_item:
+        util.LOG("PlexMod-Zidoo: Action handler called but no list item was focused.")
+        return
+
+    plex_object_str = list_item.get_property('plex_object')
+    if not plex_object_str:
+        util.LOG("PlexMod-Zidoo: Clicked item has no 'plex_object' property.")
+        return
+
+    from lib.plex import get_plex_object
+    media_object = get_plex_object(plex_object_str)
+
+    if not media_object:
+        util.LOG("PlexMod-Zidoo: Could not deserialize the plex_object.")
+        return
+
+    if media_object.is_media:
+        util.LOG("PlexMod-Zidoo: Identified playable media. Calling PLAYER.playVideo.")
+        PLAYER.playVideo(media_object, resume=True)
     else:
-        # addon not started
-        if not getGlobalProperty('started'):
-            # we're waiting for the addon to start, immediate start was requested
-            if getGlobalProperty('waiting_for_start'):
-                setGlobalProperty('waiting_for_start', '', wait=True)
-                log('Main: script.plexmod.zidoo: Currently waiting for start, immediate start was requested.')
-                sys.exit(0)
+        util.LOG("PlexMod-Zidoo: Item is not playable media, navigating...")
+        util.navigate(media_object)
 
-            # only allow a single instance
-            with SingleInstance("pm4k"):
-                started = True
-                skip_ensure_home = False
-                from lib import main
 
-                # called from service.py?
-                if from_kiosk:
-                    waited = 0
-                    if boot_delay:
-                        set_waiting_for_start = True
-                        setGlobalProperty('waiting_for_start', '1', wait=True)
-                        log('Main: script.plexmod.zidoo: Delaying start for {}s.', boot_delay)
-                        while (not main.util.MONITOR.abortRequested() and waited < boot_delay
-                               and getGlobalProperty('waiting_for_start')):
-                            waited += 0.1
-                            main.util.MONITOR.waitForAbort(0.1)
-
-                    # boot delay canceled by immediate start
-                    if waited < boot_delay:
-                        log('Main: script.plexmod.zidoo: Forced start before auto-start delay ({:.1f}/{} s).',
-                            waited, boot_delay)
-                        skip_ensure_home = True
-
-                    if kiosk_always:
-                        skip_ensure_home = True
-
-                    waited = 0
-                    # wait 120s if we're not at home right now or have an active dialog until starting the addon
-                    if not skip_ensure_home and \
-                            (xbmcgui.getCurrentWindowId() > 10000 or xbmcgui.getCurrentWindowDialogId() > 9999):
-                        setGlobalProperty('waiting_for_start', '1', wait=True)
-
-                        while getGlobalProperty('waiting_for_start') and not main.util.MONITOR.abortRequested() and \
-                                waited < 120 and (
-                                xbmcgui.getCurrentWindowId() > 10000 or xbmcgui.getCurrentWindowDialogId() > 9999):
-                            if waited == 0:
-                                log('Main: script.plexmod.zidoo: Waiting for auto-start; we\'re not home or have an '
-                                    'active dialog.')
-                            waited += 0.1
-                            main.util.MONITOR.waitForAbort(0.1)
-
-                        if from_kiosk:
-                            # no immediate start requested
-                            main.util.MONITOR.waitForAbort(0.5)
-
-                setGlobalProperty('waiting_for_start', '', wait=True)
-                setGlobalProperty('started', '1', wait=True)
-
-                main.main()
-        else:
-            log('Main: script.plexmod.zidoo: Already running, exiting')
-
-except SingleInstanceException:
-    pass
-
-except SystemExit as e:
-    if e.code not in (-1, 0):
-        raise
-
-finally:
-    if started:
-        setGlobalProperty('started', '')
-    if set_waiting_for_start:
-        setGlobalProperty('waiting_for_start', '')
+if __name__ == '__main__':
+    try:
+        plugin.run()
+    except Exception as e:
+        util.ERROR(e, notify=False)
